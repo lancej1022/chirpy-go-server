e133714fcba298318ad787d5134cbfdc21db1b80
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-02-25 17:56:15 -0700

    Add basic healthcheck endpoint

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func main() {
 	// It matches the URL of each incoming request against a list of registered patterns
 	// and calls the handler for the pattern that most closely matches the URL.
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", healthcheck)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +22,10 @@ func main() {
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
